bc22ee4c5d6a2a7389cfc3006c5b8f5ee407704a
Maxime S <Maxmtlfr@gmail.com>
2022-01-15 16:23:22 +0100

    Début de page product + index

--- front/js/product.js
+++ front/js/product.js
@@ -1,2 +1,33 @@
 //Afficher les produits
+const url = 'http://localhost:3000/api/products/';
 
+// http://localhost.../id?=126440
+const getId = new URL(window.location.href);
+const id = getId.searchParams.get('id');
+
+
+
+
+// Récupérer l'id du produit que le visiteur visite
+// const id = ;
+
+// Récupérer le produit en question  Fonction ( fetch )
+// http://localhost:3000/api/products/ID
+
+
+
+function productId(){
+
+    fetch(url + id).then((data) =>
+    data.json()
+    .then((data) => {
+
+        console.log(data)
+     }
+    ))
+
+     
+}
+productDetail();
+
+//Recupérer la couleur et l'ajouter via js 
--- front/js/script.js
+++ front/js/script.js
@@ -1,49 +1,35 @@
 // Récupérer les produits
-const url = 'http://localhost:3000/api/products';
+const url = "http://localhost:3000/api/products/";
 
-function getProducts(){
 
-    fetch(url).then((data) => 
+
+function getProducts() {
+  fetch(url).then((data) =>
     data.json()
-    .then((data) => {
-      
-        
-        for(let i = 0; i < data.length; i++){
-            let name = data[i].name;
-            let imageUrl = data[i].imageUrl;
-            let price = data[i].price;
-            let id =  data[i]._id 
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          let name = data[i].name;
+          let imageUrl = data[i].imageUrl;
+          let price = data[i].price;
+          let id = data[i]._id;
+          let description = data[i].description;
+          let altTxt = data[i].altTxt; //Toutes les infos du produit ont été récupérées sauf la couleur dont l'option de trouve sur la page produit.
 
-            console.log(data[i])
-            
-            let items = `<a href="./product.html?id=${id}">
-            <article>
-              <img src="${imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
-              <h3 class="productName">${name}</h3>
-              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-            <p>Prix : ${price}€</p>
-              </article>
-          </a>`;
-
-          document.querySelector('.items').innerHTML += items;
-
+          console.log(data[i]);
+          //afficher les produits via innerHTML
+          let items = `<a href="./product.html?id=${id}">
+                          <article>
+                            <img src="${imageUrl}" alt="${altTxt}">
+                            <h3 class="productName">${name}</h3>
+                            <p class="productDescription">${description}</p>
+                            <p>Prix : ${price}€</p>
+                          </article>
+                        </a>`;
+          document.querySelector(".items").innerHTML += items;
         }
-
-
-    })
-    )
-
+      })
+  );
 }
 
 getProducts();
-
-
-
-
-
-
-
-
-
-
 
