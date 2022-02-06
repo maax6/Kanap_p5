c8bebaa4673f699c579d81267185a496e3f44264
Maxime S <Maxmtlfr@gmail.com>
2022-02-06 15:35:19 +0100

    if quantity... début du localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -2,7 +2,7 @@
 const newUrl = new URL(window.location.href);
 //isoler l'ID dans l'URL
 let ID = newUrl.searchParams.get('id');
-// l'URL pour fetch s'apelle urlPRoduct
+// l'URL pour fetch s'apelle urlProduct
 const urlProduct = `http://localhost:3000/api/products/${ID}`;
 //La fonction suivante va chercher les infos de l'item ciblé par ID sur l'API, les traduit en JSON et intègre le html nécéssaire
 function getProductDetails() {
@@ -33,35 +33,32 @@ getProductDetails()
 
 
 // Enregistrer le produit dans le localStorage sous la clé cart "cart".
-
-   
-  
-   document.getElementById('addToCart').addEventListener("click", (event)=>{
-    event.preventDefault();
-    let price = document.querySelector("#price").value;
-    let chosenColor = document. querySelector("#colors").value;
-    let chosenQuantity = document.querySelector("#quantity").value;
-    let name = document.querySelector("#name").value;
-    let description = document.querySelector("#description").value;
-    let image = document.querySelector(".item__img").value;
-    if (chosenQuantity > 0 || chosenQuantity <=100 ){
-      let article = {
-        ID,
-        chosenColor,
-        chosenQuantity,
-        name,
-        description,
-        image
-        
+document.getElementById('addToCart').addEventListener("click", (event)=>{
+  event.preventDefault();
+  // let price = document.querySelector("#price").value;
+  let chosenColor = document. querySelector("#colors").value;
+  let chosenQuantity = document.querySelector("#quantity").value;
+  // let nameItem = document.querySelector("#name").value;NONEED
+  // let description = document.querySelector("#description").value;NONEED
+  // let image = document.querySelector(".item__img").value; NONEED
+  if (chosenQuantity > 0 || chosenQuantity <=100 ){
+    let article = {
+      ID,
+      chosenColor,
+      chosenQuantity,
+      // nameItem, NONEED
+      // description,NONEED
+      // image  NONEED
     }
+    console.log("l'objet \"article\" est defini");
+
     let productLocalStorage = JSON.parse(localStorage.getItem("cart")) ||[];
     let found = false;
     for (let product of productLocalStorage){
-      
       // if ( id && SAMEID && meme couleur, modifier quantité + quantité à ajouter){
       // afficher l'info avec console.log()
-      if (ID === productLocalStorage.ID && colors === productLocalStorage.ID colorsOption) {
-        chosenQuantity = parseInt(chosenQuantity) + parseInt(productLoalStorage.quantity);
+       if (ID === productLocalStorage.ID && colors === productLocalStorage.chosenColor) {
+        chosenQuantity = parseInt(chosenQuantity) + parseInt(productLoalStorage.chosenQuantity);
         found = true;
         break;
       }
@@ -74,20 +71,19 @@ getProductDetails()
         idProduct,
         chosenColor,
         chosenQuantity,
-       // nomProduit: data.name,
-       // descriptionProduit: data.description,
-       // imgProduit: data.imageUrl,
-       // altImgProduit: data.altTxt
-   }
-   productLocalStorage.push(article);
+        // imgProduit: data.imageUrl,
+        // altImgProduit: data.altTxt
+     }
+
+
+    productLocalStorage.push(article);
     }
     localStorage.setItem("cart", JSON.stringify(productLocalStorage));
-
   };
 
     // cart.localStorage.setItem.parse(cart);
-  //Récupérer Price, Couleur, et quantité.
-  // les stocker dans le localstorage.
-  // localStorage.setItem("cart"), JSON.parse(cart);
+    //Récupérer Price, Couleur, et quantité.
+    // les stocker dans le localstorage.
+    // localStorage.setItem("cart"), JSON.parse(cart);
   });
 
