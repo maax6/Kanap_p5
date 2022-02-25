15c547a7aa34319abc67f84b4c9200ddc202df66
Maxime S <Maxmtlfr@gmail.com>
2022-02-25 20:49:20 +0100

    prices & quantities LOG ok
    have to push total in totalPrice Array and apply reduce

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,16 +1,19 @@
 const cartArray = JSON.parse(localStorage.getItem("cart"));
-let totalPrice = 0;
+let totalPrice = [];
+let totalItem = [];
 // console.log(cartArray) //(4) [{…}, {…}, {…}, {…}] Localstorage
 // let mapID = cartArray.map((item) => item.ID);
 // let mapColors = cartArray.map((item) => item.chosenColor);
 // let mapQty = cartArray.map((item) => item.chosenQuantity);
 const productsApi = "http://localhost:3000/api/products/";
 const promiseProduct = fetch(productsApi);
-promiseProduct.then((response) =>
-  response.json().then((products) => {
+promiseProduct
+.then((response) =>
+  response.json()
+  .then((products) => {
     // console.log(products);
     for (let i = 0; i < cartArray.length; i++) {
-    // let product = false;
+      // let product = false;
       for (let j = 0; j < products.length; j++) {
         if (cartArray[i] !== undefined && products[j]._id !== undefined) {
           if (products[j]._id == cartArray[i].ID) {
@@ -28,9 +31,7 @@ promiseProduct.then((response) =>
                 /**************/
                 /*Display HTML*/
                 /**************/
-            let displayItem = `<article class="cart__item" data-id="${id}" data-color="${color}" id="${
-              cartArray[i].ID + "-" + cartArray[i].chosenColor
-            }">
+            let displayItem = `<article class="cart__item" data-id="${id}" data-color="${color}" data-qty ="${Qty}">
                                     <div class="cart__item__img">
                                       <img src="${imageUrl}">
                                     </div>
@@ -38,11 +39,11 @@ promiseProduct.then((response) =>
                                       <div class="cart__item__content__description">
                                         <h2>${name}</h2>
                                         <p>${color}</p>
-                                        <p data-price ="${price}" >${price + "€"}</p>
+                                        <p class="price" data-price ="${price}" >${price + "€"}</p>
                                       </div>
                                       <div class="cart__item__content__settings">
                                         <div class="cart__item__content__settings__quantity">
-                                          <p data-qty ="${Qty}" >Qté : ${Qty}</p>
+                                          <p>Qté : ${Qty}</p>
                                           <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${Qty}">
                                         </div>
                                         <div class="cart__item__content__settings__delete">
@@ -56,6 +57,7 @@ promiseProduct.then((response) =>
                                     </div>
                                   </article>`;
             document.querySelector("#cart__items").innerHTML += displayItem;
+            totalItem.push(Qty)
           }
         }
       }
@@ -78,82 +80,142 @@ promiseProduct.then((response) =>
             if (confirm("Cet article sera supprimé de votre panier")) {
               closeArticle.remove();//supprimer le tag article
               cartArray.splice(index, 1 ); // on retire ce canapé du panier
-              ;
-             }
+            }
           }
         });
         localStorage.setItem("cart", JSON.stringify(cartArray));  
       });
     });
-    /******************/
-    /*Modify Quantity**/
-    /******************/
+  /******************/
+  /*Modify Quantity**/
+  /******************/
+  const btnChangeQty = document.querySelectorAll(".itemQuantity");
+  btnChangeQty.forEach((btn) => {
+    const closeArticle = btn.closest("article");
+    const id = closeArticle.dataset.id;
+    // console.log(id)
+    let quantityUpdate = "";
+    const color = closeArticle.dataset.color;
+    btn.addEventListener("change", (event) => {
+      event.preventDefault();
+      quantityUpdate = Number(btn.value);
+      console.log("new Quantity has been set to "+ quantityUpdate + " for this article")
+      cartArray.forEach((couch) => {
+        if (couch.chosenColor == color && couch.ID == id) {
+         couch.chosenQuantity = quantityUpdate;
+         if(couch.chosenQuantity == 0){
+          let index = cartArray.indexOf(couch);
+          if (confirm("Cet article sera supprimé de votre panier")) {
+            closeArticle.remove();//supprimer le tag article
+            cartArray.splice(index, 1 ); // on retire ce canapé du panier
+            ;
+           }
+         }
+        }
+      });
+      localStorage.setItem("cart", JSON.stringify(cartArray));
+    });
+  });
 
     /******************/
     /*total  Quantity**/
     /******************/
     
-    
-    
-    
-    
-    let totalItem = [];
     function itemQuantity(cartArray) {
       for (let couch of cartArray) {
         let itemQuantity = couch.chosenQuantity
         totalItem.push(itemQuantity);
-        console.log(totalItem)
-        const reducer =(previousValue, currentValue) => previousValue + currentValue;
+        // console.log(totalItem)
+        const reducer =(previousValue, currentValue) => parseInt(previousValue) + parseInt(currentValue);
         const totalQuantity = totalItem.reduce(reducer);
-           console.log(totalQuantity)
+          //  console.log(totalQuantity)
          document.getElementById("totalQuantity").innerText = totalQuantity;
-        } 
+      } 
        if (totalItem.length === 0) {
          document.querySelector("h1").innerText = "Votre panier est vide";
          totalQuantity = "";
          document.getElementById("totalQuantity").innerText = totalItem;
        }
     }
-    
-    /**⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆/
-     /FAUT LES METTRE ICI/
-     /⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆*/
-     itemQuantity(cartArray);
+      itemQuantity(cartArray);
+
+      /****************/
+      /*Display Price**/
+      /****************/
+
+      
+      /****************/
+      /*Display Price**/
+      /****************/
+
+
+
+
+
+
+
+
+
+
+      let priceTag = document.querySelectorAll(".price")
+      priceTag.forEach((price) => {
+        const closeArticle = price.closest("article")
+        console.log(closeArticle)
+        const closePrice = price.closest(".price")
+        const priceProduct = closePrice.dataset.price
+        console.log(priceProduct)
+        const qtyValue = closeArticle.dataset.qty
+        console.log(qtyValue)
+        const closeQty = price.closest(".qty");
+
+        let totalPrice = priceProduct * qtyValue;
+
+        console.log(totalPrice)
+        
+      })
+
+
+      
+      // let priceByItem = parseInt(qtyItem) * parseInt(priceTag);
+      //        console.log(qty
+      
+        // for (let couch of cartArray) {
+        //  let itemPrice = cartArray.chosenQuantity;
+        //  let totalPrice = priceElem * couch.chosenQuantity;
+
+        // }
+      /**⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆/
+       /⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆*/
   })
+
+
+  
 );
-    
 
 
 
-/****************/
-/*Display Price**/
-/*********/
-/*à placer dans la bonne fonction */
+// let apiArray = [];
+// .then(apiResponse => {
+//   // Récupérer tous les produits de l'API
+//   for (let allProducts of apiResponse){
+//       apiArray.push(AllProducts)
+//   };
+// })
 
-// let prices = document.querySelectorAll(' .itemQuantity');
-// console.log(price.value);
-
-//Cibler toutes les values des input itemQuantity
-
-//Mettre les valeurs dans un tableau
-
-//reduce le tableau
-
-// .innerHTML += reducedPrice
 
 
 
 /*********************/
 /*Display Total Price
 
-    const priceProduct = document.dataset()
+    const closePrice = document.dataset()
     cartArray.forEach((couch) => {
       if (couch.ID == id){
       console.log(couch.ID)
     }
       totalPriceQuantity = products.price * couch.chosenQuantity;
-        priceArray.push(totalPriceQuantity);
- 
+      priceArray.push(totalPriceQuantity);
+      
         document.getElementById("totalPrice").innerText = totalPrice;
       })
 **********************/
